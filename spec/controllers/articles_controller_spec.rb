3bfa0ef1e910aaac3f8cf935b3a8ebf904ecd6df
DanielTseng <artsppdaniel@gmail.com>
2017-01-12 09:55:10 +0800

    ok

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -14,8 +14,8 @@ RSpec.describe ArticlesController, type: :controller do
       it "renders the edit template" do
         article = Fabricate(:article, author: @user)
         stub_user_sign_in(@user)
-        get :edit, params: {id: article}
 
+        get :edit, params: {id: article}
         expect(response).to render_template :edit
         expect(response).to have_http_status(:success)
 
@@ -28,16 +28,39 @@ RSpec.describe ArticlesController, type: :controller do
       end
     end
 
+    context "owner is allowed to delete other users' articles" do
+      it "redirect to the root path" do
+        article = Fabricate(:article, author: @user)
+        stub_user_sign_in(@user)
+
+        delete :destroy, params: {id: article}
+        expect(response).to redirect_to(articles_path)
+        expect(flash[:success]).to eq("Article has been deleted")
+      end
+    end
+
+
     context "non-owner is not allowed to edit other users' articles" do
       it "redirect to the root path" do
         article = Fabricate(:article, author: @user)
         stub_user_sign_in(@other_user)
-        get :edit, params: {id: article}
 
+        get :edit, params: {id: article}
         expect(response).to redirect_to(root_path)
         expect(flash[:warning]).to eq("Permission denied!")
 
-        put :edit, params: {id: article, title: "xxxxxx", body: "xxxxxx"}
+        put :update, params: {id: article, article: {title: "Updated Title", body: "Updated Body"}}
+        expect(response).to redirect_to(root_path)
+        expect(flash[:warning]).to eq("Permission denied!")
+      end
+    end
+
+    context "non-owner is not allowed to delete other users' articles" do
+      it "redirect to the root path" do
+        article = Fabricate(:article, author: @user)
+        stub_user_sign_in(@other_user)
+
+        delete :destroy, params: {id: article}
         expect(response).to redirect_to(root_path)
         expect(flash[:warning]).to eq("Permission denied!")
       end
